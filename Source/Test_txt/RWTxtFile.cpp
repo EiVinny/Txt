010fd4ccb18a3344924bd9d8e11837975502265c
EiVinny <38143255+EiVinny@users.noreply.github.com>
2018-05-10 11:51:12 -0300

    1.2

--- Source/Test_txt/RWTxtFile.cpp
+++ Source/Test_txt/RWTxtFile.cpp
@@ -4,12 +4,52 @@
 
 bool URWTxtFile::LoadTxt(FString FileNameA, FString& SaveTextA)
 {
-	return FFileHelper::LoadFileToString(SaveTextA, *(FPaths::GameDir() + FileNameA));
+	enum { FILEREAD_AllowWrite = 0x04};
+	//AllowWrite = 0x04;
+	return FFileHelper::LoadFileToString(SaveTextA, *(FPaths::GameDir() + FileNameA), EFileRead::FILEREAD_AllowWrite);
 }
 
 bool URWTxtFile::SaveTxt(FString SaveTextB, FString FileNameB)
 {
-	return FFileHelper::SaveStringToFile(SaveTextB, *(FPaths::GameDir() + FileNameB));
+	enum { FILEWRITE_Append = 0x08};
+	return FFileHelper::SaveStringToFile(SaveTextB, *(FPaths::GameDir() + FileNameB), FFileHelper::EEncodingOptions::AutoDetect, &IFileManager::Get(), EFileWrite::FILEWRITE_Append);
 }
 
+bool URWTxtFile::SaveArray(TArray<uint8> Array, FString FileNameC)
+{
+	enum { FILEWRITE_Append = 0x08};
+	return FFileHelper::SaveArrayToFile(Array, *(FPaths::GameDir() + FileNameC), &IFileManager::Get(), EFileWrite::FILEWRITE_Append);
+}
 
+bool URWTxtFile::LoadArray(FString& FilenameD,TArray<uint8> ArrayB)
+{
+	return FFileHelper::LoadFileToArray(ArrayB, *(FPaths::GameDir() + FilenameD));
+}
+
+void URWTxtFile::CarregarTexto(FString FileNameE,  FVector&VetorLegal)
+{
+	//Funciona
+	/*
+	using namespace std;
+	float numero;
+	float numero2;
+	float numero3;
+	FVector Vetor = FVector(0.0f, 0.0f, 0.0f);
+	ofstream output;
+	output.open("NewLocation.txt");
+	output << 121.1 << " " << 122.2 << " " << 123.3;
+	output.close();
+	ifstream input;
+	input.open ("NewLocation.txt");
+	input >> numero;
+	input >> numero2;
+	input >> numero3;
+	Vetor.X = numero;
+	Vetor.Y = numero2;
+	Vetor.Z = numero3;
+	VetorLegal = Vetor;
+	input.close();
+	*/
+	//Acrescentar Vetor, Carregar o texto inteiro
+
+}
--- Source/Test_txt/RWTxtFile.h
+++ Source/Test_txt/RWTxtFile.h
@@ -1,8 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
+//#include "CoreMinimal.h"
+#include "fstream"
+#include "iostream"
+#include "list"
 
-#include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "RWTxtFile.generated.h"
 
@@ -12,15 +15,25 @@
 UCLASS()
 class TEST_TXT_API URWTxtFile : public UBlueprintFunctionLibrary
 {
+	private:
+		//list<float> *ilist = new list<float>(); 
+
 	GENERATED_BODY() public:
 
 		UFUNCTION(BlueprintPure, Category = "Custom", meta = (Keywords = "LoadTxt"))
 			static bool LoadTxt(FString FileNameA, FString& SaveTextA);
-
+			
 		UFUNCTION(BlueprintCallable, Category = "Custom", meta = (Keywords = "SaveTxt"))
 			static bool SaveTxt(FString SaveTextB, FString FileNameB);
+
+		UFUNCTION(BlueprintCallable, Category = "Custom", meta = (Keywords = "SaveArray"))
+			static bool SaveArray(TArray<uint8> Array, FString FileNameC);
 	
-	
+		UFUNCTION(BlueprintPure, Category = "Custom", meta = (Keywords = "LoadArray"))
+			static bool LoadArray(FString& FileNameD,TArray<uint8> ArrayB);
+
+		UFUNCTION(BlueprintPure, Category = "Custom", meta = (Keywords = "CarregarTexto"))
+			static void CarregarTexto(FString FileNameE, FVector&VetorLegal);
 	
 	
 };
